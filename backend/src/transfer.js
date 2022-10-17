f7c07a331c8e7f757c93b0a09ad096a45a52288e
nidhi S <nidhi.sharma@iaastha.com>
2022-10-17 12:23:47 +0530

    feature : send transaction

--- backend/src/transfer.js
+++ backend/src/transfer.js
@@ -1,38 +1,355 @@
-import * as dotenv from 'dotenv' 
-import Web3 from "web3";
-dotenv.config()
+const Web3 = require("web3");
+//import Web3 from 'web3';
 
 async function main() {
-  // Configuring the connection to the Polygon node
-  const network = process.env.POLYGON_NETWORK;
-  const web3 = new Web3(
-    new Web3.providers.HttpProvider(
-      `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`
-    )
-  );
-  // Creating a signing account from a private key
-  const signer = web3.eth.accounts.privateKeyToAccount(
-    process.env.SIGNER_PRIVATE_KEY
-  );
-  web3.eth.accounts.wallet.add(signer);
-  // Creating the transaction object
-  const tx = {
-    from: signer.address,
-    to: "0xeAD9C93b79Ae7C1591b1FB5323BD777E86e150d4",
-    value: web3.utils.toWei("0.001"),
-  };
-  // Assigning the right amount of gas
-  tx.gas = await web3.eth.estimateGas(tx);
+  const web3 = new Web3("https://api.avax-test.network/ext/bc/C/rpc");
+  const myAddress = "0xFC38390A8621c76c4e5e7b5451a844995086dAfa"; //TODO: replace this address with your own public address
 
-  // Sending the transaction to the network
-  const receipt = await web3.eth
-    .sendTransaction(tx)
-    .once("transactionHash", (txhash) => {
-      console.log(`Mining transaction ...`);
-      console.log(`Transaction hash: ${txhash}`);
-    });
-  // The transaction is now on chain!
-  console.log(`Mined in block ${receipt.blockNumber}`);
+  const nonce = await web3.eth.getTransactionCount(myAddress, "latest"); // nonce starts counting from 0
+
+  const abi = [
+    { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "approved",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "Approval",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "operator",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "bool",
+          name: "approved",
+          type: "bool",
+        },
+      ],
+      name: "ApprovalForAll",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "minter",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "MintNFT",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "beforeAddr",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "afterAddr",
+          type: "address",
+        },
+      ],
+      name: "MinterSet",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "previousOwner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "newOwner",
+          type: "address",
+        },
+      ],
+      name: "OwnershipTransferred",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        { indexed: true, internalType: "address", name: "to", type: "address" },
+        {
+          indexed: true,
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "Transfer",
+      type: "event",
+    },
+    {
+      inputs: [],
+      name: "Minter",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "to", type: "address" },
+        { internalType: "uint256", name: "tokenId", type: "uint256" },
+      ],
+      name: "approve",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "owner", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
+      name: "getApproved",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getTokenCount",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "owner", type: "address" },
+        { internalType: "address", name: "operator", type: "address" },
+      ],
+      name: "isApprovedForAll",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "minter", type: "address" },
+        { internalType: "string", name: "metaURI", type: "string" },
+      ],
+      name: "mintNFT",
+      outputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "name",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "owner",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
+      name: "ownerOf",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "renounceOwnership",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "from", type: "address" },
+        { internalType: "address", name: "to", type: "address" },
+        { internalType: "uint256", name: "tokenId", type: "uint256" },
+      ],
+      name: "safeTransferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "from", type: "address" },
+        { internalType: "address", name: "to", type: "address" },
+        { internalType: "uint256", name: "tokenId", type: "uint256" },
+        { internalType: "bytes", name: "_data", type: "bytes" },
+      ],
+      name: "safeTransferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "operator", type: "address" },
+        { internalType: "bool", name: "approved", type: "bool" },
+      ],
+      name: "setApprovalForAll",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "minter", type: "address" }],
+      name: "setMinter",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "bytes4", name: "interfaceId", type: "bytes4" }],
+      name: "supportsInterface",
+      outputs: [{ internalType: "bool", name: "", type: "bool" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "symbol",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
+      name: "tokenURI",
+      outputs: [{ internalType: "string", name: "", type: "string" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "address", name: "from", type: "address" },
+        { internalType: "address", name: "to", type: "address" },
+        { internalType: "uint256", name: "tokenId", type: "uint256" },
+      ],
+      name: "transferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+      name: "transferOwnership",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "wallet", type: "address" }],
+      name: "withdraw",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    { stateMutability: "payable", type: "receive" },
+  ];
+
+  const contractAddress = "0xFb470DFbd969Ed0858a61EC5d7Fb8CB96ea12692";
+
+  const contractInstance = new web3.eth.Contract(abi, contractAddress);
+
+  const data = await await contractInstance.methods
+    .mintNFT("0xFC38390A8621c76c4e5e7b5451a844995086dAfa", "aa")
+    .encodeABI();
+
+  const transaction = {
+    to: "0xFb470DFbd969Ed0858a61EC5d7Fb8CB96ea12692", // faucet address to return eth
+    value: 0,
+    gas: 3000000,
+    nonce: nonce,
+    data,
+    // optional data field to send message or execute smart contract
+  };
+
+  const signedTx = await web3.eth.accounts.signTransaction(
+    transaction,
+    "4ace70b48bba7d1e2a29974c68fb7f1a11d4fa1979e234fe4eca54ff30df85c5"
+  );
+
+  web3.eth.sendSignedTransaction(
+    signedTx.rawTransaction,
+    function (error, hash) {
+      if (!error) {
+        console.log(
+          "🎉 The hash of your transaction is: ",
+          hash,
+          "\n Check Alchemy's Mempool to view the status of your transaction!"
+        );
+      } else {
+        console.log(
+          "❗Something went wrong while submitting your transaction:",
+          error
+        );
+      }
+    }
+  );
 }
 
 main();
