64dbaed74ee30b93aa86052589e2e570d3fa2107
naruto0913 <abe.sinjiro7@gmail.com>
2022-11-01 02:36:16 +0900

    update db connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,8 +2,15 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-      const conn = await mongoose.connect(process.env.MONGO_URI)
+    const mongoURI = process.env.MONGO_URI || "";
+
+    if(mongoURI.length > 0) {
+      const conn = await mongoose.connect(mongoURI);
       console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } else {
+      console.log("Not set mongodb connect");
+      process.exit(1)
+    }
   } catch (error) {
     console.log(error)
     process.exit(1)
@@ -11,4 +18,3 @@ const connectDB = async () => {
 }
 
 export default connectDB
-
