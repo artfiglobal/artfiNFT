8eaac0077b715b624233b5c64132c63782072b19
naruto0913 <abe.sinjiro7@gmail.com>
2022-11-17 01:55:29 +0900

    fix: error in whitelist controller

--- backend/src/controllers/waitlistController.ts
+++ backend/src/controllers/waitlistController.ts
@@ -19,29 +19,28 @@ export const sendMailToUser = asyncHandler(async (req, res) => {
     const existingEmail = await waitlistModel.findOne({ email })
     if(existingEmail && existingEmail.email != "") {
       res.status(500).json({ message: "You email was already registed"});
+    } else {
+      const referCode = referral && referral.length > 0 ? referral : getReferCode();
+      const res2 = await sendMailUsingSendGrid({
+        to: email,
+        from: process.env.WAITLIST_SENDER,
+        subject: "Thank you for joining the waitlist for Artfi NFT offerings!",
+        template: "waitlist",
+        templateVars: {
+          referCode,
+        },
+      });
+
+      const createWaitlist = new waitlistModel(
+      {
+          email: email,
+          referralCode: referCode,
+          subsciption: subscription,
+      });
+      await createWaitlist.save();
+
+      res.status(200).json({ message: "Mail Sent", res: res2 });
     }
-
-    const referCode = referral && referral.length > 0 ? referral : getReferCode();
-    
-    const res2 = await sendMailUsingSendGrid({
-      to: email,
-      from: process.env.WAITLIST_SENDER,
-      subject: "Thank you for joining the waitlist for Artfi NFT offerings!",
-      template: "waitlist",
-      templateVars: {
-        referCode,
-      },
-    });
-
-    const createWaitlist = new waitlistModel(
-    {
-        email: email,
-        referralCode: referCode,
-        subsciption: subscription,
-    });
-    await createWaitlist.save();
-
-    res.status(200).json({ message: "Mail Sent", res: res2 });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error", error });
   }
