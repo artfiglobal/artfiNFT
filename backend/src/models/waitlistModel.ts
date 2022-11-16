d49dfc6083a840f5c4b3141fecacc3a91e75889c
John <johnpatch0502@outlook.com>
2022-11-16 16:21:51 +0700

    feat: store waitlist info in the db

---
+++ backend/src/models/waitlistModel.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const waitlistSchema = new mongoose.Schema({
+  
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    referralCode: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    subscription: {
+      type: Boolean,
+    }
+});
+
+const waitlistModel = mongoose.model('artist' , waitlistSchema)
+export default waitlistModel
