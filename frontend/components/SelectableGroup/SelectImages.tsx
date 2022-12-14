db51ac99d51ad39678a347fa0602b11575a9abb1
kimo2121 <kareem.gaad@gmail.com>
2022-12-14 21:00:12 +0200

    removed fake fractions

--- frontend/components/SelectableGroup/SelectImages.tsx
+++ frontend/components/SelectableGroup/SelectImages.tsx
@@ -5,26 +5,7 @@ import style from "./fractions.module.scss";
 // import Image from "next/image";
 // import { Typography } from "../reusables2/Atoms/Typography/index";
 // import style from "./Landing.module.scss";
-import image11 from "../../public/images/New folder/row-1-column-1.png";
-import image12 from "../../public/images/New folder/row-1-column-2.png";
-import image13 from "../../public/images/New folder/row-1-column-3.png";
-import image14 from "../../public/images/New folder/row-1-column-4.png";
-import image15 from "../../public/images/New folder/row-1-column-5.png";
-import image16 from "../../public/images/New folder/row-1-column-6.png";
-import image17 from "../../public/images/New folder/row-1-column-7.png";
-import image18 from "../../public/images/New folder/row-1-column-8.png";
-import image19 from "../../public/images/New folder/row-1-column-9.png";
-import image110 from "../../public/images/New folder/row-1-column-10.png";
-import image21 from "../../public/images/New folder/row-2-column-1.png";
-import image22 from "../../public/images/New folder/row-2-column-2.png";
-import image23 from "../../public/images/New folder/row-2-column-3.png";
-import image24 from "../../public/images/New folder/row-2-column-4.png";
-import image25 from "../../public/images/New folder/row-2-column-5.png";
-import image26 from "../../public/images/New folder/row-2-column-6.png";
-import image27 from "../../public/images/New folder/row-2-column-7.png";
-import image28 from "../../public/images/New folder/row-2-column-8.png";
-import image29 from "../../public/images/New folder/row-2-column-9.png";
-import image210 from "../../public/images/New folder/row-2-column-10.png";
+
 export default function SelectFractionNFTs({
   isShown,
   setIsShown,
@@ -37,28 +18,7 @@ export default function SelectFractionNFTs({
   // const [coords, setCoords] = useState([0, 0]);
   const [singleImage, setSingleImage] = useState();
   const [NoSelectedItems, setNoSelectedItems] = useState(false);
-  const images = [
-    image11,
-    image12,
-    image13,
-    image14,
-    image15,
-    image16,
-    image17,
-    image18,
-    image19,
-    image110,
-    image21,
-    image22,
-    image23,
-    image24,
-    image25,
-    image26,
-    image27,
-    image28,
-    image29,
-    image210,
-  ];
+
   const getRandomNumber = (min = 50, max = 200) => {
     return Math.random() * (max - min) + min;
   };
