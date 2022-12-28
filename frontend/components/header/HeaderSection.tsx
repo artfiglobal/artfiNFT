e34d54e85e4eb80968b3ecc4813257cace7aa79d
ankush <ankushsss99gmail.com>
2022-12-28 16:03:34 +0530

    fix:navbar

--- frontend/components/header/HeaderSection.tsx
+++ frontend/components/header/HeaderSection.tsx
@@ -12,6 +12,7 @@ const HeaderSection = ({ header }: any) => {
           background: "url(/Background/Header.png)",
           width: "100%",
           height: "450px",
+          marginTop: "80px"
         }}
       >
         <h1>Discover & Collect Artfi NFT</h1>
