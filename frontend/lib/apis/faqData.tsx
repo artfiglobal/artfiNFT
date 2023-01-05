2e265c0185707684a0c7b46884a06b038cfc542b
ankush <ankushsss99gmail.com>
2023-01-06 03:41:31 +0530

    fix faq

--- frontend/lib/apis/faqData.tsx
+++ frontend/lib/apis/faqData.tsx
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const faqData = async () => {
   const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_React_App_Base_Url}/api/faq/getallfaq`
+    `${process.env.NEXT_PUBLIC_React_App_Base_Url}/api/faq/getFAQbypositionindex`
   );
-  const data = await response.data.FAQlist
+  const data = await response.data.list
   console.log(data)
   return data;
 };
