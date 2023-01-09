77ff5044e5eddfd0f609ff37ad106c7bb905e95d
naruto0913 <abe.sinjiro7@gmail.com>
2023-01-10 01:41:06 +0900

    feat: update missing part

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+export type FormDataInterface = {
+  address: string | undefined;
+  contractSigned: boolean;
+  amount: number;
+  email: string | undefined;
+  chain: string;
+  termsSignature: string;
+  selCnt: number;
+};
