d97cb79f3885b278725fadd0215f2ebd9d4b3e1c
naruto0913 <abe.sinjiro7@gmail.com>
2023-01-09 21:53:27 +0900

    fix: updat sending mail

--- frontend/components/Home2/Landing/index.tsx
+++ frontend/components/Home2/Landing/index.tsx
@@ -293,14 +293,7 @@ LandingProps | any): JSX.Element => {
 
                 await axios.post(
                   `${process.env.NEXT_PUBLIC_React_App_Base_Url}/api/whitelist/send-email`,
-                  { body: emailAddress },
-                  {
-                    headers: {
-                      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImthcmVlbUBnbWFpbC5jb20iLCJyb2xlIjoic3VwZXJhZG1pbiIsImlkIjoiNjNhNTcwNmNkNmQ3MTU1ZDc1ZTg2NzUyIiwiaWF0IjoxNjcyMzg4Njc2LCJleHAiOjE2NzIzOTU4NzZ9.b5Lmajxbd6k26sJvTI4LBPYyO2En0Xb3Ng8XxIHQ7SM`,
-                      "Content-Type": "application/json",
-                      "Content-Length": "<calculated when request is sent>",
-                    },
-                  }
+                  { body: emailAddress }
                 );
               }).catch(function (err1: any) {
                 setConfirmPurchase(false)
